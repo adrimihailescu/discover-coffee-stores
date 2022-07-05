57359112dd2268e110cf0bb7ce43080dfd0c593f
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-05 15:55:48 +0100

    add google font Roboto

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-no-undef */
+import Document, { Head, Html, Main, NextScript } from "next/dist/pages/_document";
+
+class MyDocument extends Document {
+render() {
+    return (
+        <Html lang="en">
+        <Head>
+            <link rel="preload" href="/fonts/Roboto-Bold.ttf" as="font" crossOrigin="anonymous"></link>
+            <link rel="preload" href="/fonts/Roboto-Medium.ttf" as="font" crossOrigin="anonymous"></link>
+            <link rel="preload" href="/fonts/Roboto-Regular.ttf" as="font" crossOrigin="anonymous"></link>
+
+        </Head>
+        <body>
+            <Main></Main>
+            <NextScript />
+        </body>
+    </Html>
+    );
+};
+};
+
+export default MyDocument;
+  
+           
+        
