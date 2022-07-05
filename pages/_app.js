0aef82b27f805033f3287fdf503a6e2d252ab3c9
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-05 12:36:06 +0100

    setup project,banner component and basic css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <Component {...pageProps} />
+    <footer>
+      <p>2022 Adriana</p>
+    </footer>
+  </div>);
 }
 
 export default MyApp
