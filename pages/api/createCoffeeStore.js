7d6a09fcebb97885b52235e6040621084ec748aa
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-11 17:36:51 +0100

    add airtable to create coffee store API, error handling and refactor code

---
+++ pages/api/createCoffeeStore.js
@@ -0,0 +1,58 @@
+import { table, getMinifiedRecords } from "../../lib/airtable";
+
+console.log({ table });
+
+const createCoffeeStore = async (req, res) => {
+	if (req.method === "POST") {
+		//find a record
+
+		const { id, name, neighborhood, address, imgUrl, voting } = req.body;
+		try {
+			if (id) {
+				const findCoffeeStoreRecords = await table
+					.select({
+						filterByFormula: `id=${id}`,
+					})
+					.firstPage();
+
+				if (findCoffeeStoreRecords.length !== 0) {
+					const records = getMinifiedRecords(findCoffeeStoreRecords);
+
+					res.json(records);
+				} else {
+					//create a record
+
+					if (name) {
+						const createRecords = await table.create([
+							{
+								fields: {
+									id,
+									name,
+									address,
+									neighborhood,
+									voting,
+									imgUrl,
+								},
+							},
+						]);
+
+						const records = getMinifiedRecords(createRecords);
+						res.json({ records });
+					} else {
+						res.status(400);
+						res.json({ message: "Name or id is missing" });
+					}
+				}
+			} else {
+				res.status(400);
+				res.json({ message: "Id is missing" });
+			}
+		} catch (err) {
+			console.error("Error creating or finding store", err);
+			res.status(500);
+			res.json({ message: "Error creating or finding store", err });
+		}
+	}
+};
+
+export default createCoffeeStore;
