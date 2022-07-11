cce37cf56e800cffa426ff8da59562d1df53c118
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-11 11:33:18 +0100

    add useContext hook for coffeeStores and latLong

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import Image from "next/dist/client/image";
 import coffeeStoresData from "../data/coffee-stores.json";
 import { fetchCoffeeStores } from "../lib/coffee-stores";
 import useTrackLocation from "../hooks/use-track-location";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { ACTION_TYPES, StoreContext } from "./_app";
 
 //server side
 export async function getStaticProps(context) {
@@ -24,11 +25,16 @@ export async function getStaticProps(context) {
 export default function Home(props) {
 	console.log("props", props);
 
-	const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
+	const { handleTrackLocation, locationErrorMsg, isFindingLocation } =
 		useTrackLocation();
 
-	const [coffeeStores, setCoffeeStores] = useState("");
+	// const [coffeeStores, setCoffeeStores] = useState("");
 	const [coffeeStoresError, setCoffeeStoresError] = useState(null);
+
+	const { dispatch, state } = useContext(StoreContext);
+
+	const { coffeeStores, latLong } = state;
+
 	console.log({ latLong, locationErrorMsg });
 
 	useEffect(() => {
@@ -37,7 +43,13 @@ export default function Home(props) {
 				try {
 					const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
 					console.log({ fetchedCoffeeStores });
-					setCoffeeStores(fetchedCoffeeStores);
+					// setCoffeeStores(fetchedCoffeeStores);
+					dispatch({
+						type: ACTION_TYPES.SET_COFFEE_STORES,
+						payload: {
+							coffeeStores: fetchedCoffeeStores,
+						},
+					});
 					//set coffee stores
 				} catch (error) {
 					//set error
