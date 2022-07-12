f08e0bb16694f894a695aac45e3f58e419dd8b49
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-12 13:26:13 +0100

    add logic for invoking API for favourite coffee store

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -120,10 +120,29 @@ const CoffeeStore = (initialProps) => {
 		}
 	}, [data]);
 
-	const handleUpvoteButton = () => {
+	const handleUpvoteButton = async () => {
 		console.log("handle upvote!");
-		let count = votingCount + 1;
-		setVotingCount(count);
+		try {
+			const response = await fetch("/api/favouriteCoffeeStoreById", {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					id,
+				}),
+			});
+
+			const dbCoffeeStore = await response.json();
+			console.log(dbCoffeeStore);
+
+			if (dbCoffeeStore && dbCoffeeStore.length > 0) {
+				let count = votingCount + 1;
+				setVotingCount(count);
+			}
+		} catch (err) {
+			console.error("Error upvoting coffee store", err);
+		}
 	};
 
 	if (error) {
